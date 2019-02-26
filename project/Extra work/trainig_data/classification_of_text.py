832e8a9a49b9c043bfb1754aef63aa49e3ff8511
shraddhansu panda <47125559+shraddhansupanda@users.noreply.github.com>
2019-02-26 06:19:46 +0530

    Add files via upload

---
+++ project/Extra work/trainig_data/classification_of_text.py
@@ -0,0 +1,17 @@
+import pandas as pd
+from sklearn.svm import SVC
+from generation_training_data import *
+import numpy as np
+import spacy
+nlp=spacy.load('en')
+Y=X.reshape(-1,384)
+y=input()
+y=str(y)
+doc=nlp(y)
+doc1=doc.vector
+doc2=np.array(doc1)
+doc3=doc1.reshape(-1,384)
+svm=SVC()
+svm.fit(Y,label)
+pred=svm.predict(doc3)
+print(pred)
