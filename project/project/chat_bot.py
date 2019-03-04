24771e1f441227cfdd44cc7139e011cea044db66
shraddhansu panda <47125559+shraddhansupanda@users.noreply.github.com>
2019-03-05 00:59:55 +0530

    Add files via upload

--- project/project/chat_bot.py
+++ project/project/chat_bot.py
@@ -2,7 +2,6 @@ from new_function import *
 import re
 print('                      welcome to Data Analysis Chat-Bot')
 print('                             Ask your question')
-
 #To make the country in lower case
 con=[x.lower() for x in list(country_name())]
 #To make the program excecute repeatedly
@@ -14,7 +13,7 @@ while True:
     try:
         #To find year in the string
         def year():
-            pattern=re.compile('\d{4}')
+            pattern=re.compile(r'\b\d{4}\b')
             years=re.findall(pattern,x)
             if len(years) == 1:
                 year=int(years[0])
@@ -25,11 +24,14 @@ while True:
                 return year,year1
             if len(years)==0:
                 return 1800,2016
-        def position():
-            pattern=re.compile('\d{1,3}')
-            pos=re.findall(pattern,x)
-            pos=pos[0]
-            return pos
+        #to find the bins for histogram in the string
+        def bin():
+            pattern=re.compile(r'\b\d{1,3}\b')
+            bins=re.findall(pattern,x)
+            if len(bins) == 1:
+                return int(bins[0])
+            else:
+                return 20
         #To find country name in the string
         def common_member():
             a_set = set(y)
@@ -43,22 +45,22 @@ while True:
         if 'scatter' in y and 'plot' in y:
             scatter_plot(common_member(),*year())
         if 'histogram' in y:
-            histogram(common_member(),*year())
+            histogram(common_member(),*year(),int(bin()))
         if 'graphical' in y and 'eda' in y:
             graphical_EDA(common_member()[0],*year())
         if 'average' in y or 'avg' in y:
-            avg(common_member()[0],*year())
+            avg(common_member(),*year())
         if 'variance' in y or 'var' in y:
-            variance(common_member()[0],*year())
+            variance(common_member(),*year())
         if 'standard deviation' in y or 'std' in y:
-            standard_deviation(common_member()[0],*year())
+            standard_deviation(common_member(),*year())
         if ('statistical' in y and 'eda' in y) or ('statistical' and 'exploritory' and 'data' and 'analysis' in y):
-            statistical_EDA(common_member()[0],*year())
+            statistical_EDA(common_member(),*year())
         if 'what' in y and (('life' in y and'expectancy' in y) or 'le' in y) and 'predict' not in y and 'will' not in y and 'maximum' not in y and 'highest' not in y and 'lowest' not in y and 'minimum' not in y:
-            what_is(common_member()[0],year())
+            what_is(common_member(),year())
         if 'what' in y and ('highest' in y or 'maximum' in y or 'max' in y) and (('life' in y and 'expectancy' in y) or 'le' in y):
             highest_life_expectancy_of_country(common_member()[0])
-        if 'what' in y and ('lowest' in y or 'minimum' in y) and (('life' in y and 'expectancy' in y) or 'le' in y):
+        if 'what' in y and ('lowest' in y or 'minimum' in y or 'min' in y) and (('life' in y and 'expectancy' in y) or 'le' in y):
             lowest_life_expectancy_of_country(common_member()[0])
         if 'which' in y and 'country' in y and ('highest' in y or 'maximum' in y) and (('life' in y and 'expectancy' in y) or 'le' in y) and 'on' in y:
             country_highest_life_expectancy(year())
@@ -66,9 +68,9 @@ while True:
             country_lowest_life_expectancy(year())
         if 'which' in y and 'country' in y and ('highest' in y or 'maximum' in y) and (('life' in y and 'expectancy' in y) or 'le' in y) and 'on' not in y:
             highest_life_expectancy()
-        if 'which' in y and 'country' in y and ('lowest' in y or 'minimum' in y) and (('life' in y and 'expectancy' in y) or 'le' in y) and 'on' not in y:
+        if 'which' in y and 'country' in y and ('lowest' in y or 'minimum' in y or 'min' in y) and (('life' in y and 'expectancy' in y) or 'le' in y) and 'on' not in y:
             lowest_life_expectancy()
         if 'predict' in y or 'will' in y:
-            predict(common_member()[0],year())
+            predict(common_member(),year())
     except:
         print('Please Enter correctly with proper meaning')
--- project/project/new_function.py
+++ project/project/new_function.py
@@ -2,7 +2,6 @@ import sqlalchemy
 import re
 import matplotlib.pyplot as plt
 import pandas as pd
-import seaborn as sns
 import numpy as np
 import time
 from sklearn.linear_model import LinearRegression
@@ -11,113 +10,70 @@ def connect():
     connection=engine.connect()
     return connection
 def country_name():
-    connection=connect()
-    df=pd.read_sql('select * from life where year=0',connection)
+    df=pd.read_sql('select * from life where year=0',connect())
     df=df.iloc[:,1:]
     df=list(df)
     return df
-def box_plot(country,year=1800,year1=2016):
-    if len(country)==1:
-        country=country[0]
-        connection=connect()
-        df=pd.read_sql('select {} from life where year between {} AND {}'.format(country,year,year1),connection)
-        plt.boxplot(df['{}'.format(country)])
-        plt.grid()
-        plt.xlabel(country)
-        plt.ylabel('year')
-        plt.title('boxplot of {}'.format(country))
-        plt.show()
-    if len(country)==2:
-        country1=country[0]
-        country2=country[1]
-        connection=connect()
-        df=pd.read_sql('select {} from life where year between {} AND {}'.format(country1,year,year1),connection)
-        grid=plt.GridSpec(1,2)
-        plt.subplot(grid[0,0])
+def box_plot(country,year,year1):
+    grid=plt.GridSpec(1,len(country))
+    for x in range(len(country)):
+        country1=country[int('{}'.format(x))]
+        df=pd.read_sql('select {} from life where year between {} AND {}'.format(country1,year,year1),connect())
+        plt.subplot(grid[0,x])
         plt.boxplot(df['{}'.format(country1)])
         plt.grid()
-        plt.xlabel(country1)
+        plt.xlabel('{}'.format(country1))
         plt.ylabel('year')
         plt.title('boxplot of {}'.format(country1))
-        plt.subplot(grid[0,1])
-        df=pd.read_sql('select {} from life where year between {} AND {}'.format(country2,year,year1),connection)
-        plt.boxplot(df['{}'.format(country2)])
-        plt.grid()
-        plt.xlabel(country2)
-        plt.ylabel('year')
-        plt.title('boxplot of {}'.format(country2))
-        plt.tight_layout()
-        time.sleep(0.3)
-        plt.show()
-def scatter_plot(country,year=1800,year1=2016):
-    if len(country)==1:
-        country=country[0]
-        connection=connect()
-        df=pd.read_sql('select {} from life where year between {} AND {}'.format(country,year,year1),connection)
+    plt.tight_layout()
+    time.sleep(0.3)
+    plt.show()
+def scatter_plot(country,year,year1):
+    grid=plt.GridSpec(len(country),1)
+    for x in range(len(country)):
+        country1=country[int('{}'.format(x))]
+        df=pd.read_sql('select {} from life where year between {} AND {}'.format(country1,year,year1),connect())
         df.index=[x for x in np.arange(year,year1+1)]
         #plt.style.use('ggplot')
-        plt.plot(df.index,df['{}'.format(country)])
-        plt.grid()
-        plt.xlabel('year')
-        plt.ylabel('life_expectancy')
-        plt.title('Scatter plot of {}'.format(country))
-        time.sleep(0.3)
-        plt.show()
-    if len(country)==2:
-        connection=connect()
-        country1=country[0]
-        country2=country[1]
-        df=pd.read_sql('select {} from life where year between {} AND {}'.format(country1,year,year1),connection)
-        df.index=[x for x in np.arange(year,year1+1)]
-        #plt.style.use('ggplot')
-        grid=plt.GridSpec(2,1)
-        plt.subplot(grid[0,0])
+        plt.subplot(grid[x,0])
         plt.plot(df.index,df['{}'.format(country1)])
         plt.grid()
         plt.xlabel('year')
         plt.ylabel('life_expectancy')
         plt.title('Scatter plot of {}'.format(country1))
-        df=pd.read_sql('select {} from life where year between {} AND {}'.format(country2,year,year1),connection)
-        df.index=[x for x in np.arange(year,year1+1)]
-        #plt.style.use('ggplot')
-        plt.subplot(grid[1,0])
-        plt.plot(df.index,df['{}'.format(country2)])
-        plt.grid()
-        plt.xlabel('year')
-        plt.ylabel('life_expectancy')
-        plt.title('Scatter plot of {}'.format(country2))
-        plt.tight_layout()
-        time.sleep(0.3)
-        plt.show()
-def histogram(country,year=1800,year1=2016,bins=20):
-    connection=connect()
-    df=pd.read_sql('select {} from life where year between {} AND {}'.format(country,year,year1),connection)
-    plt.hist(df['{}'.format(country)],bins)
-    plt.title('Histogram of {}'.format(country))
-    plt.xlabel('Life expectancy')
-    plt.grid()
+    plt.tight_layout()
     time.sleep(0.3)
     plt.show()
-def graphical_EDA(country,year=1800,year1=2016):
+def histogram(country,year,year1,bins):
+    grid=plt.GridSpec(len(country),1)
+    for x in range(len(country)):
+        country1=country[int('{}'.format(x))]
+        df=pd.read_sql('select {} from life where year between {} AND {}'.format(country1,year,year1),connect())
+        plt.subplot(grid[x,0])
+        plt.hist(df['{}'.format(country1)],bins)
+        plt.title('Histogram of {}'.format(country1))
+        plt.xlabel('Life expectancy')
+        plt.grid()
+    plt.tight_layout()
+    time.sleep(0.3)
+    plt.show()
+def graphical_EDA(country,year,year1):
     grid=plt.GridSpec(2,2)
     plt.subplot(grid[0,0])
-    connection=connect()
-    df=pd.read_sql('select {} from life where year between {} AND {}'.format(country,year,year1),connection)
+    df=pd.read_sql('select {} from life where year between {} AND {}'.format(country,year,year1),connect())
     plt.boxplot(df['{}'.format(country)])
     plt.grid()
     plt.xlabel(country)
     plt.ylabel('year')
     plt.title('boxplot of {}'.format(country))
     plt.subplot(grid[0,1])
-    connection=connect()
-    df=pd.read_sql('select {} from life where year between {} AND {}'.format(country,year,year1),connection)
+    df=pd.read_sql('select {} from life where year between {} AND {}'.format(country,year,year1),connect())
     plt.hist(df['{}'.format(country)],bins=20)
     plt.title('Histogram of {}'.format(country))
     plt.xlabel('Life expectancy')
     plt.grid()
     plt.subplot(grid[1,0:])
-    connection=connect()
-    df=pd.read_sql('select {} from life where year between {} AND {}'.format(country,year,year1),connection)
+    df=pd.read_sql('select {} from life where year between {} AND {}'.format(country,year,year1),connect())
     df.index=[x for x in np.arange(year,year1+1)]
     #plt.style.use('ggplot')
     plt.plot(df.index,df['{}'.format(country)])
@@ -128,66 +84,67 @@ def graphical_EDA(country,year=1800,year1=2016):
     plt.tight_layout()
     time.sleep(0.3)
     plt.show()
-def avg(country,year=1800,year1=2016):
-    connection=connect()
-    df=pd.read_sql('select AVG({}) from life where year between {} AND {}'.format(country,year,year1),connection)
-    time.sleep(0.3)
-    if year==1800 and year1==2016:
-        print('{} is the Average of {}'.format(float(df.values),country))
-    else:
-        print('{} is the Average of {} from {} to {}'.format(float(df.values),country,year,year1))
-def variance(country,year=1800,year1=2016):
-    connection=connect()
-    df=pd.read_sql('select VARIANCE({}) from life where year between {} AND {}'.format(country,year,year1),connection)
-    time.sleep(0.3)
-    if year==1800 and year1==2016:
-        print('{} is the variance of {}'.format(float(df.values),country))
-    else:
-        print('{} is the variance of {} from {} to {}'.format(float(df.values),country,year,year1))
-def standard_deviation(country,year=1800,year1=2016):
-    connection=connect()
-    df=pd.read_sql('select STDDEV({}) from life where year between {} AND {}'.format(country,year,year1),connection)
-    time.sleep(0.3)
-    if year==1800 and year1==2016:
-        print('{} is the Standard deviation of {}'.format(float(df.values),country))
-    else:
-        print('{} is the Standard deviation of {} from {} to {}'.format(float(df.values),country,year,year1))
+def avg(country,year,year1):
+    for x in range(len(country)):
+        country1=country[int('{}'.format(x))]
+        df=pd.read_sql('select AVG({}) from life where year between {} AND {}'.format(country1,year,year1),connect())
+        time.sleep(0.3)
+        if year==1800 and year1==2016:
+            print('{} is the Average of {}'.format(float(df.values),country1))
+        else:
+            print('{} is the Average of {} from {} to {}'.format(float(df.values),country1,year,year1))
+def variance(country,year,year1):
+    for x in range(len(country)):
+        country1=country[int('{}'.format(x))]
+        df=pd.read_sql('select VARIANCE({}) from life where year between {} AND {}'.format(country1,year,year1),connect())
+        time.sleep(0.3)
+        if year==1800 and year1==2016:
+            print('{} is the variance of {}'.format(float(df.values),country1))
+        else:
+            print('{} is the variance of {} from {} to {}'.format(float(df.values),country1,year,year1))
+def standard_deviation(country,year,year1):
+    for x in range(len(country)):
+        country1=country[int('{}'.format(x))]
+        df=pd.read_sql('select STDDEV({}) from life where year between {} AND {}'.format(country1,year,year1),connect())
+        time.sleep(0.3)
+        if year==1800 and year1==2016:
+            print('{} is the Standard deviation of {}'.format(float(df.values),country1))
+        else:
+            print('{} is the Standard deviation of {} from {} to {}'.format(float(df.values),country1,year,year1))
 def statistical_EDA(country,year=1800,year1=2016):
-    avg(country,year,year1)
-    variance(country,year,year1)
-    standard_deviation(country,year,year1)
+    for x in range(len(country)):
+        country1=country[int('{}'.format(x))]
+        avg([country1],year,year1)
+        variance([country1],year,year1)
+        standard_deviation([country1],year,year1)
 def what_is(country,year):
-    connection=connect()
-    df=pd.read_sql('select {} from life where year = {}'.format(country,year),connection)
-    time.sleep(0.3)
-    print('{} is the life_expectancy of {} on {}'.format(float(df.values),country,year))
+    for x in range(len(country)):
+        country1=country[int('{}'.format(x))]
+        df=pd.read_sql('select {} from life where year = {}'.format(country1,year),connect())
+        time.sleep(0.3)
+        print('{} is the life_expectancy of {} on {}'.format(float(df.values),country1,year))
 def highest_life_expectancy_of_country(country):
-    connection=connect()
-    df=pd.read_sql('select year,{} from life where {}=(select max({}) from life)'.format(country,country,country),connection)
+    df=pd.read_sql('select year,{} from life where {}=(select max({}) from life)'.format(country,country,country),connect())
     time.sleep(0.3)
     print('{} is the max life expectancy for {} on {}'.format(df['{}'.format(country)].values[0],country,df['year'].values[0]))
 def lowest_life_expectancy_of_country(country):
-    connection=connect()
-    df=pd.read_sql('select year,{} from life where {}=(select min({}) from life)'.format(country,country,country),connection)
+    df=pd.read_sql('select year,{} from life where {}=(select min({}) from life)'.format(country,country,country),connect())
     time.sleep(0.3)
     print('{} is the min life expectancy for {} on {}'.format(df['{}'.format(country)].values[0],country,df['year'].values[0]))
 def country_highest_life_expectancy(year):
-    connection=connect()
-    df=pd.read_sql('select * from life where year={}'.format(year),connection)
+    df=pd.read_sql('select * from life where year={}'.format(year),connect())
     df=df.iloc[0,1:][df.iloc[0,1:]==df.iloc[0,1:].max()]
     for i in range(len(df)):
         time.sleep(0.5)
         print('{} has highest Life_expectancey of {} on {}'.format(df.index[i],df[i],year))
 def country_lowest_life_expectancy(year):
-    connection=connect()
-    df=pd.read_sql('select * from life where year={}'.format(year),connection)
+    df=pd.read_sql('select * from life where year={}'.format(year),connect())
     df=df.iloc[0,1:][df.iloc[0,1:]==df.iloc[0,1:].min()]
     for i in range(len(df)):
         time.sleep(0.5)
         print('{} has lowest Life_expectancey of {} on {}'.format(df.index[i],df[i],year))
 def highest_life_expectancy():
-    connection=connect()
-    df=pd.read_sql('select * from life',con=connection)
+    df=pd.read_sql('select * from life',connect())
     df1=df.iloc[:,1:]
     df2=df1.max()
     df3=df2[df2==df2.max()]
@@ -195,8 +152,7 @@ def highest_life_expectancy():
         time.sleep(0.5)
         print('{} has highest life expectancy of {}'.format(df3.index[i],df3[i]))
 def lowest_life_expectancy():
-    connection=connect()
-    df=pd.read_sql('select * from life',con=connection)
+    df=pd.read_sql('select * from life',connect())
     df.index=df.iloc[:,0]
     df1=df.iloc[:,1:]
     df2=df1.min()
@@ -206,12 +162,13 @@ def lowest_life_expectancy():
         time.sleep(0.5)
         print('{} has lowest life expectancy of {} on {}'.format(df3.index[i],df3[i],year))
 def predict(country,year):
-    connection=connect()
-    df=pd.read_sql('select year,{} from life where year between 1971 and 2016'.format(country),connection)
-    x=df['year'].values.reshape(-1,1)
-    y=df.iloc[:,1].values.reshape(-1,1)
-    reg=LinearRegression()
-    reg.fit(x,y)
-    k=float(reg.predict(year))
-    time.sleep(0.3)
-    print('{} is the life expectancy for {} on {}'.format(k,country,str(year)))
+    for x in range(len(country)):
+        country1=country[int('{}'.format(x))]
+        df=pd.read_sql('select year,{} from life where year between 1971 and 2016'.format(country1),connect())
+        x=df['year'].values.reshape(-1,1)
+        y=df.iloc[:,1].values.reshape(-1,1)
+        reg=LinearRegression()
+        reg.fit(x,y)
+        k=float(reg.predict(year))
+        time.sleep(0.3)
+        print('{} is the life expectancy for {} on {}'.format(k,country1,str(year)))
